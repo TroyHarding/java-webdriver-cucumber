b03381fe1311cd2cc6c19eaa6390213a0522f28b
Troy Harding <ozzyqa@gmail.com>
2023-06-19 16:18:02 -0700

    "Added updated TestContext and Yaml"

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -1,6 +1,7 @@
 // Created by Viacheslav (Slava) Skryabin 04/01/2011
 package support;
 
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
@@ -18,32 +19,103 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestContext {
 
     private static WebDriver driver;
+
+    private static Map<String, Object> testData = new HashMap<>();
+
+    public static void setTestData(String key, Object value) {
+        testData.put(key, value);
+    }
+
+    public static String getStringTestData(String key) {
+        return (String) testData.get(key);
+    }
+
+    public static Integer getIntegerTestData(String key) {
+        return (Integer) testData.get(key);
+    }
 
     public static WebDriver getDriver() {
         return driver;
     }
 
     public static void initialize() {
-        initialize("chrome", "local", false);
+        initialize(getConfig().getString("browser"), getConfig().getBoolean("isHeadless"));
     }
-
     public static void teardown() {
         driver.quit();
     }
 
-    public static void initialize(String browser, String testEnv, boolean isHeadless) {
+    public static YamlMap getConfig() {
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(getFile("config", "yml"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return new YamlMap(new Yaml().load(stream));
+    }
+
+    public static File getFile(String fileName, String extension) {
+        String path = System.getProperty("user.dir") + "/src/test/resources/data/" + fileName + "." + extension;
+        return new File(path);
+    }
+
+    public static void initialize(String browser, boolean isHeadless) {
         Dimension size = new Dimension(1920, 1080);
         Point position = new Point(0, 0);
-        if (testEnv.equals("local")) {
+        if (getConfig().getString("infrastructure").equals("grid")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.ANY);
+            try {
+                URL hubUrl = new URL("http://localhost:4444/wd/hub");
+                driver = new RemoteWebDriver(hubUrl, capabilities);
+                ((RemoteWebDriver) driver).setFileDetector(new LocalFileDetector());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        } else if (getConfig().getString("infrastructure").equals(("selenoid"))) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.LINUX);
+            capabilities.setVersion(getConfig().getDouble("browserVersion").toString());
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            capabilities.setCapability("sessionTimeout", "5m");
+            capabilities.setCapability("idleTimeout", "300");
+            capabilities.setCapability("videoScreenSize", "1920x1080");
+            capabilities.setCapability("screenResolution", "1920x1080");
+
+            String testName = getConfig().getString("env") + " " + getStringTestData("scenarioTag") + " \"" + getStringTestData("scenarioName") + "\"";
+            capabilities.setCapability("name", testName);
+            String videoName = String.join("", testName.replaceAll("[^A-Za-z0-9]", "_"), "_", String.valueOf(new Date().getTime()), ".mp4");
+            setTestData("videoName", videoName);
+            capabilities.setCapability("videoName", videoName);
+            URL hubUrl = null;
+            try {
+                hubUrl = new URL("http://" + getConfig().getString("hubUrl") + ":4444/wd/hub");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            driver = new RemoteWebDriver(hubUrl, capabilities);
+        } else {
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
@@ -98,19 +170,17 @@ public class TestContext {
                 default:
                     throw new RuntimeException("Driver is not implemented for: " + browser);
             }
-        } else if (testEnv.equals("grid")){
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName(browser);
-            capabilities.setPlatform(Platform.ANY);
-            try {
-                URL hubUrl = new URL("http://localhost:4444/wd/hub");
-                driver = new RemoteWebDriver(hubUrl, capabilities);
-                ((RemoteWebDriver) driver).setFileDetector(new LocalFileDetector());
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static void setScenarioData(Scenario scenario) {
+        TestContext.setTestData("scenarioName", scenario.getName());
+        for (String tag : scenario.getSourceTagNames()) {
+            Matcher matcher = Pattern.compile("@[\\w_]+\\d+").matcher(tag);
+            if (matcher.find()) {
+                TestContext.setTestData("scenarioTag", matcher.group(0));
+                break;
             }
-        } else {
-            throw new RuntimeException("Unsupported test environment: " + testEnv);
         }
     }
 }
